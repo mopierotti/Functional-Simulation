d66d745bdcb6145a18154750bf5e7340dfc83ea0
mopierotti <mopierotti@gmail.com>
2014-11-03 10:04:48 -0800

    Add implementation of Simulation

---
+++ src/main/scala/mopierotti/fsim/Simulation.scala
@@ -0,0 +1,36 @@
+package mopierotti.fsim
+
+object Simulation {
+  type Time = Int
+  trait Configuration
+  trait State
+  case class SimulationResult(states:Seq[State])
+  trait AggregatedSimulationResults
+
+  def runSimulation(conf:Configuration,
+                    startingState:State,
+                    endTime:Time,
+                    createStepFunction:(Configuration => ((State, Time) => State))):SimulationResult = {
+    val startingState = startingState
+    val stepFunction = createStepFunction(conf)
+    val states = (1 to endTime).scanLeft(startingState)(stepFunction)
+    SimulationResult(states)
+  }
+
+  def runNTimesAndAggregateResults(n:Int,
+                                  conf:Configuration,
+                                  startingState:State,
+                                  endTime:Time,
+                                  createStepFunction:(Configuration => ((State, Time) => State)),
+                                  createAggregatedResults:(Seq[SimulationResult] => AggregatedSimulationResults)):AggregatedSimulationResults = {
+    val results = (1 to n).map(? => runSimulation(conf, startingState, endTime, createStepFunction))
+    createAggregatedResults(results)
+  }
+
+  case class DefaultAggregatedResults(results:Seq[SimulationResult]) extends AggregatedSimulationResults
+
+  def defaultAggregator(results:Seq[SimulationResult]):AggregatedSimulationResults = {
+    DefaultAggregatedResults(results)
+  }
+
+}
